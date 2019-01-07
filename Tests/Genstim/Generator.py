50bbe9c2b0a9653e5cae05cc5d631d14d4ad7a0e
Koroteyka <nu.koroteeva@gmail.com>
2019-01-07 21:07:48 +0300

    удалила комменты

--- Tests/Genstim/Generator.py
+++ Tests/Genstim/Generator.py
@@ -1,15 +1,6 @@
 import numpy as np
 import random
-# make and print out two-dimensional array from 0 to 9 using function np.random.random_integers, 3 arguments (min, max, size)
-# mylist = np.random.random_integers(0, 9, (2, 10)) # initialize variable c with function np.random.random_integers and print out result
-# print (mylist) # print and check the result
-# compare all numbers on first line using for loop
 
-
-# initialize one-dimensional array from 0 to 9 using function np.random.random_integers, 3 arguments (min, max, size)
-#mylist = np.random.random_integers(0, 9, 10)
-#print out result and check the result
-#print (mylist)
 
 m=10
 mass=[]
@@ -17,10 +8,10 @@ corrans=[]
 k=0
 
 for i in range(m):
-   mass.append(random.randint(0,10)) 
+   mass.append(random.randint(0,10))
    print(mass[i])
 for i in range(m):
-   corrans.append(random.randint(0,10)) 
+   corrans.append(random.randint(0,10))
    print(corrans[i])
 
 for i in range(8):
